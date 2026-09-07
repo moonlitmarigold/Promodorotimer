0941a43d5f2906b8091d6f220ba77ccd0af4c953
Shardalchik <maksimshwydeko@gmail.com>
2026-09-07 21:55:10 +0200

    UIV2

--- src/promodotimer/app.py
+++ src/promodotimer/app.py
@@ -1,48 +1,96 @@
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
+from .timer import Timer
 
-class PromodoWindow(QtWidgets.QMainWindow):
+class PomodoroWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         # Variables
-        self.timer = self.return_timer()
+
+        self.timer_logic = self.return_timer_logic()
+
+        self.timer_label = self.return_timer_label()
         self.start_button = self.return_timer_start_button()
         self.settings_button = self.return_settings_button()
 
         # Layout
 
-        #MainScreen
+        # MainScreen
+
+        # Layout horizontal mid
+
+        self.horizontal_mid_layout = QtWidgets.QHBoxLayout()
+
+        # Layout vertical bottom
+
+        self.vertical_bottom_layout_settings = QtWidgets.QVBoxLayout()
+
+        self.vertical_bottom_layout_settings.addStretch()
+        self.vertical_bottom_layout_settings.addWidget(self.settings_button)
+
+        self.horizontal_mid_layout.addLayout(self.vertical_bottom_layout_settings)
 
         # Layout vertical mid
-        self.vertical_mid_layout = QtWidgets.QVBoxLayout(self)
+        self.vertical_mid_layout = QtWidgets.QVBoxLayout()
         self.vertical_mid_layout.setSpacing(15)
 
+        self.preset_label = QtWidgets.QLabel("Presets")
+         #Presets
+        self.presets_grid = QtWidgets.QGridLayout()
+
+        preset_1 = QtWidgets.QLabel("megaPreset1")
+        preset_2 = QtWidgets.QLabel("megaPreset2")
+        preset_3 = QtWidgets.QLabel("megaPreset3")
+        preset_4 = QtWidgets.QPushButton("+")
+
+
+        self.presets_grid.addWidget(preset_1, 0, 0)
+        self.presets_grid.addWidget(preset_2, 0, 1)
+        self.presets_grid.addWidget(preset_3, 0, 2)
+        self.presets_grid.addWidget(preset_4, 1, 1)
+
         self.vertical_mid_layout.addStretch()
-        self.vertical_mid_layout.addWidget(self.timer, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.vertical_mid_layout.addWidget(self.timer_label, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
         self.vertical_mid_layout.addWidget(self.start_button, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.vertical_mid_layout.addWidget(self.preset_label, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.vertical_mid_layout.addLayout(self.presets_grid)
         self.vertical_mid_layout.addStretch()
 
-        # Layout horizontal_bottom
+        self.horizontal_mid_layout.addStretch(2)
+        self.horizontal_mid_layout.addLayout(self.vertical_mid_layout)
 
-        self.horizontal_bottom_layout = QtWidgets.QHBoxLayout()
-        self.horizontal_bottom_layout.addWidget(self.settings_button)
-        self.horizontal_bottom_layout.addStretch()
+        # Layout vertical mid tasks
 
-        self.vertical_mid_layout.addLayout(self.horizontal_bottom_layout)
+        self.vertical_mid_layout_tasks = QtWidgets.QVBoxLayout()
+        tasks_label = QtWidgets.QLabel("Tasks")
 
-        #Settings Screen
+
+        task_1 = QtWidgets.QLabel("megaTask1")
+        task_2 = QtWidgets.QLabel("megaTask2")
+        task_3 = QtWidgets.QPushButton("+")
+
+        self.vertical_mid_layout_tasks.addStretch()
+        self.vertical_mid_layout_tasks.addWidget(tasks_label, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.vertical_mid_layout_tasks.addWidget(task_1, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.vertical_mid_layout_tasks.addWidget(task_2, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.vertical_mid_layout_tasks.addWidget(task_3, alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.vertical_mid_layout_tasks.addStretch()
+
+        self.horizontal_mid_layout.addStretch(1)
+        self.horizontal_mid_layout.addLayout(self.vertical_mid_layout_tasks)
+        self.horizontal_mid_layout.addStretch(1)
+
+        # Settings Screen
         self.vertical_mid_layout_set = QtWidgets.QVBoxLayout()
-        toggle = QtWidgets.QLabel("This is not a button")
+        toggle = QtWidgets.QLabel("This is not a button", alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
         self.vertical_mid_layout_set.addWidget(toggle)
 
-
-
-        #Finish
+        # Finish
         self.central_widget = QtWidgets.QStackedWidget()
 
         self.main_page = QtWidgets.QWidget()
-        self.main_page.setLayout(self.vertical_mid_layout)
+        self.main_page.setLayout(self.horizontal_mid_layout)
 
         self.settings_page = QtWidgets.QWidget()
         self.settings_page.setLayout(self.vertical_mid_layout_set)
@@ -53,15 +101,26 @@ class PromodoWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self.central_widget)
 
 
-    @staticmethod
-    def return_timer():
-        timer = QtWidgets.QLabel("25:00")
-        font = timer.font()
+
+    def return_timer_label(self):
+        timer_label = QtWidgets.QLabel("25:00")
+        font = timer_label.font()
         font.setPixelSize(36)
         font.setBold(True)
-        timer.setFont(font)
+        timer_label.setFont(font)
 
-        return timer
+        return timer_label
+
+    def return_timer_logic(self):
+        timer_logic = Timer()
+        timer_logic.set_duration_seconds( 25 * 60)  # later: from your Preset
+
+        timer_logic.tick.connect(self.on_tick)
+        timer_logic.started.connect(self.on_started)
+        timer_logic.paused.connect(self.on_paused)
+        timer_logic.finished.connect(self.on_finished)
+
+        return timer_logic
 
 
     def return_timer_start_button(self):
@@ -89,10 +148,32 @@ class PromodoWindow(QtWidgets.QMainWindow):
     def presets(self):
         ...
 
+    @staticmethod
+    def format_time(seconds: int) -> str:
+        return f"{seconds // 60:02d}:{seconds % 60:02d}"
 
     @QtCore.Slot()
     def on_start_button(self):
-        self.timer.setText("6")
+        self.timer_logic.toggle()
+
+    @QtCore.Slot(int)
+    def on_tick(self, remaining: int):
+        self.timer_label.setText(self.format_time(remaining))
+
+    @QtCore.Slot()
+    def on_started(self):
+        self.start_button.setText("Pause")
+        self.timer_label.setText(self.format_time(self.timer_logic.remaining))
+
+    @QtCore.Slot()
+    def on_paused(self):
+        self.start_button.setText("Start")
+
+    @QtCore.Slot()
+    def on_finished(self):
+        self.start_button.setText("Start")
+        self.timer_logic.reset_to_duration_seconds()
+        self.timer_label.setText(self.format_time(self.timer_logic.remaining))
 
     @QtCore.Slot()
     def on_settings_button(self):
